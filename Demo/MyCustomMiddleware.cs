801b8e6497afbdf2246e60ee123c74cfbc02395d
minhit\mithang <minhtv.1602@yahoo.com>
2018-08-23 00:45:38 +0700

    Bỏ Admin Smart thay vào Admin Vue

---
+++ Demo/MyCustomMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Demo
+{
+    // Class custom middleware cần xử lí
+    public class MyCustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MyCustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        //Không nên dùng phương thức này
+        //public Task Invoke(HttpContext httpContext)
+        //{
+        //    return _next(httpContext);
+        //}
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await httpContext.Response.WriteAsync("Chạy custom middleware "+ httpContext.Items["data"]+"<br/>");
+            await _next.Invoke(httpContext);
+            await httpContext.Response.WriteAsync("Hoàn thành custom middleware <br/>");
+        }
+    }
+
+    // Mở rộng phương thức IApplicationBuilder để có thể dùng trong Startup.cs
+    public static class MyCustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MyCustomMiddleware>();
+        }
+    }
+}
---
+++ Demo/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Demo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.Use( async (context,next) =>
+            {
+                //Gán giá trị cho middle
+                context.Items["data"] = "Value";
+                await context.Response.WriteAsync("Run 1 <br/>");
+                await next.Invoke();
+            });
+            app.UseMyCustomMiddleware();
+            app.Run(async (context) =>
+            {
+                //Nhận giá trị từ context trước
+                await context.Response.WriteAsync("Run 2 "+context.Items["data"]+"<br/>");               
+            });
+
+        }
+    }
+}
