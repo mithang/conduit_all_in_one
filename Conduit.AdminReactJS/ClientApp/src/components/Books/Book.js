0ec41e72b88756c7bd6c3c535ac52a7beac016c8
minhit\mithang <minhtv.1602@yahoo.com>
2018-08-27 00:08:17 +0700

    -Thêm admin viết bằng reactjs -Chỉnh sửa có ngôn ngữ tiếng việt

---
+++ Conduit.AdminReactJS/ClientApp/src/components/Books/Book.js
@@ -0,0 +1,174 @@
+import React, {Component} from 'react';
+import ContentWrapper from '../Layout/ContentWrapper';
+import {Container, Card, CardHeader, CardBody, CardTitle} from 'reactstrap';
+import $ from 'jquery';
+
+import Datatable from './../Tables/Datatable';
+
+class Book extends Component {
+
+    state = {
+        dtOptions1: {
+            'paging': true, // Table pagination
+            'ordering': true, // Column ordering
+            'info': true, // Bottom left status text
+            responsive: true,
+            // Text translation options Note the required keywords between underscores (e.g
+            // _MENU_)
+            oLanguage: {
+                sSearch: '<em class="fa fa-search"></em>',
+                sLengthMenu: '_MENU_ records per page',
+                info: 'Showing page _PAGE_ of _PAGES_',
+                zeroRecords: 'Nothing found - sorry',
+                infoEmpty: 'No records available',
+                infoFiltered: '(filtered from _MAX_ total records)',
+                oPaginate: {
+                    sNext: '<em class="fa fa-caret-right"></em>',
+                    sPrevious: '<em class="fa fa-caret-left"></em>'
+                }
+            }
+        }
+
+    }
+
+    // Access to internal datatable instance for customizations
+    dtInstance = dtInstance => {
+        const inputSearchClass = 'datatable_input_col_search';
+        const columnInputs = $('tfoot .' + inputSearchClass);
+        // On input keyup trigger filtering
+        columnInputs.keyup(function () {
+            dtInstance.fnFilter(this.value, columnInputs.index(this));
+        });
+    }
+
+    render() {
+        return (
+            <ContentWrapper>
+                <div className="content-heading">
+                    <div>DANH MỤC SÁCH
+                        <small>Sách được cập nhật liên tục, trên hệ thống máy chủ</small>
+                    </div>
+                </div>
+
+                <Datatable options={this.state.dtOptions1}>
+                    <table className="table table-striped my-4 w-100">
+                        <thead>
+                            <tr>
+                                <th data-priority="1">MÃ SÁCH</th>
+                                <th>TÊN SÁCH</th>
+                                <th>NĂM PHÁT HÀNH</th>
+                                <th className="sort-numeric">THỂ LOẠI</th>
+                                <th className="sort-alpha" data-priority="2">SỐ LƯỢNG XB</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="gradeX">
+                                <td>Trident</td>
+                                <td>Internet Explorer 4.0</td>
+                                <td>Win 95+</td>
+                                <td>4</td>
+                                <td>X</td>
+                            </tr>
+                            <tr className="gradeC">
+                                <td>Trident</td>
+                                <td>Internet Explorer 5.0</td>
+                                <td>Win 95+</td>
+                                <td>5</td>
+                                <td>C</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Trident</td>
+                                <td>Internet Explorer 5.5</td>
+                                <td>Win 95+</td>
+                                <td>5.5</td>
+                                <td>A</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Trident</td>
+                                <td>Internet Explorer 6</td>
+                                <td>Win 98+</td>
+                                <td>6</td>
+                                <td>A</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Trident</td>
+                                <td>Internet Explorer 7</td>
+                                <td>Win XP SP2+</td>
+                                <td>7</td>
+                                <td>A</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Trident</td>
+                                <td>AOL browser (AOL desktop)</td>
+                                <td>Win XP</td>
+                                <td>6</td>
+                                <td>A</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Gecko</td>
+                                <td>Firefox 1.0</td>
+                                <td>Win 98+ / OSX.2+</td>
+                                <td>1.7</td>
+                                <td>A</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Gecko</td>
+                                <td>Firefox 1.5</td>
+                                <td>Win 98+ / OSX.2+</td>
+                                <td>1.8</td>
+                                <td>A</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Gecko</td>
+                                <td>Firefox 2.0</td>
+                                <td>Win 98+ / OSX.2+</td>
+                                <td>1.8</td>
+                                <td>A</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Gecko</td>
+                                <td>Firefox 3.0</td>
+                                <td>Win 2k+ / OSX.3+</td>
+                                <td>1.9</td>
+                                <td>A</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Gecko</td>
+                                <td>Camino 1.0</td>
+                                <td>OSX.2+</td>
+                                <td>1.8</td>
+                                <td>A</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Gecko</td>
+                                <td>Camino 1.5</td>
+                                <td>OSX.3+</td>
+                                <td>1.8</td>
+                                <td>A</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Gecko</td>
+                                <td>Netscape 7.2</td>
+                                <td>Win 95+ / Mac OS 8.6-9.2</td>
+                                <td>1.7</td>
+                                <td>A</td>
+                            </tr>
+                            <tr className="gradeA">
+                                <td>Gecko</td>
+                                <td>Mozilla 1.6</td>
+                                <td>Win 95+ / OSX.1+</td>
+                                <td>1.6</td>
+                                <td>A</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </Datatable>
+
+            </ContentWrapper>
+        );
+    }
+
+}
+
+export default Book;
